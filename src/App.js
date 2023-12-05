b828256357f20fed3a3e98882bbeaa02ddbe28b9
md shakil <shakil16.02@gmail.com>
2023-12-06 00:53:40 +0530

    added file

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      
+        <a
+         
+        >
+          Learning CI/CD on AWS for React App using AWS Amplify
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
